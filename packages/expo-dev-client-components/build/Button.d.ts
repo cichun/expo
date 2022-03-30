ef21612be9464122fc5c69a392eac28df0a9e69e
andy <40680668+ajsmth@users.noreply.github.com>
2022-03-30 14:04:04 -0700

    fix tests and build files

--- packages/expo-dev-client-components/build/Button.d.ts
+++ packages/expo-dev-client-components/build/Button.d.ts
@@ -3,20 +3,20 @@ import { Animated } from 'react-native';
 declare const Container: React.ForwardRefExoticComponent<Pick<Animated.AnimatedProps<import("react-native").PressableProps & React.RefAttributes<import("react-native").View>> & {
     children?: React.ReactNode;
 } & {
-    margin?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "16" | "12" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "14" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "medium" | "large" | "micro" | "tiny" | "xl") | undefined;
-    mx?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "16" | "12" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "14" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "medium" | "large" | "micro" | "tiny" | "xl") | undefined;
-    my?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "16" | "12" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "14" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "medium" | "large" | "micro" | "tiny" | "xl") | undefined;
-    mb?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "16" | "12" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "14" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "medium" | "large" | "micro" | "tiny" | "xl") | undefined;
-    mt?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "16" | "12" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "14" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "medium" | "large" | "micro" | "tiny" | "xl") | undefined;
-    padding?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "16" | "12" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "14" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "medium" | "large" | "micro" | "tiny" | "xl") | undefined;
-    px?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "16" | "12" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "14" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "medium" | "large" | "micro" | "tiny" | "xl") | undefined;
-    py?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "16" | "12" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "14" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "medium" | "large" | "micro" | "tiny" | "xl") | undefined;
-    pb?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "16" | "12" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "14" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "medium" | "large" | "micro" | "tiny" | "xl") | undefined;
-    pt?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "16" | "12" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "14" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "medium" | "large" | "micro" | "tiny" | "xl") | undefined;
+    margin?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
+    mx?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
+    my?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
+    mb?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
+    mt?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
+    padding?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
+    px?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
+    py?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
+    pb?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
+    pt?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
     rounded?: "small" | "medium" | "large" | "none" | "full" | undefined;
     roundedTop?: "small" | "medium" | "large" | "none" | "full" | undefined;
     roundedBottom?: "small" | "medium" | "large" | "none" | "full" | undefined;
-    bg?: "primary" | "secondary" | "tertiary" | "ghost" | "transparent" | "default" | "disabled" | undefined;
+    bg?: "transparent" | "primary" | "secondary" | "tertiary" | "ghost" | "default" | "disabled" | undefined;
     border?: "ghost" | undefined;
     shadow?: "button" | undefined;
 } & {
@@ -24,25 +24,25 @@ declare const Container: React.ForwardRefExoticComponent<Pick<Animated.AnimatedP
         light?: (import("react-native").ViewStyle | import("react-native").TextStyle | import("react-native").ImageStyle) | undefined;
         dark?: (import("react-native").ViewStyle | import("react-native").TextStyle | import("react-native").ImageStyle) | undefined;
     } | undefined;
-}, "selectors" | keyof import("react-native").PressableProps | "key" | "bg" | "border" | "shadow" | "padding" | "margin" | "mx" | "my" | "mb" | "mt" | "px" | "py" | "pb" | "pt" | "rounded" | "roundedTop" | "roundedBottom"> & React.RefAttributes<Animated.AnimatedProps<import("react-native").PressableProps & React.RefAttributes<import("react-native").View>>>>;
+}, "selectors" | "padding" | "margin" | keyof import("react-native").PressableProps | "key" | "bg" | "border" | "shadow" | "mx" | "my" | "mb" | "mt" | "px" | "py" | "pb" | "pt" | "rounded" | "roundedTop" | "roundedBottom"> & React.RefAttributes<Animated.AnimatedProps<import("react-native").PressableProps & React.RefAttributes<import("react-native").View>>>>;
 export declare const Button: {
     Container: React.ForwardRefExoticComponent<Pick<Animated.AnimatedProps<import("react-native").PressableProps & React.RefAttributes<import("react-native").View>> & {
         children?: React.ReactNode;
     } & {
-        margin?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "16" | "12" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "14" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "medium" | "large" | "micro" | "tiny" | "xl") | undefined;
-        mx?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "16" | "12" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "14" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "medium" | "large" | "micro" | "tiny" | "xl") | undefined;
-        my?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "16" | "12" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "14" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "medium" | "large" | "micro" | "tiny" | "xl") | undefined;
-        mb?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "16" | "12" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "14" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "medium" | "large" | "micro" | "tiny" | "xl") | undefined;
-        mt?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "16" | "12" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "14" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "medium" | "large" | "micro" | "tiny" | "xl") | undefined;
-        padding?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "16" | "12" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "14" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "medium" | "large" | "micro" | "tiny" | "xl") | undefined;
-        px?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "16" | "12" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "14" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "medium" | "large" | "micro" | "tiny" | "xl") | undefined;
-        py?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "16" | "12" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "14" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "medium" | "large" | "micro" | "tiny" | "xl") | undefined;
-        pb?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "16" | "12" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "14" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "medium" | "large" | "micro" | "tiny" | "xl") | undefined;
-        pt?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "16" | "12" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "14" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "medium" | "large" | "micro" | "tiny" | "xl") | undefined;
+        margin?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
+        mx?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
+        my?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
+        mb?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
+        mt?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
+        padding?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
+        px?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
+        py?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
+        pb?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
+        pt?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
         rounded?: "small" | "medium" | "large" | "none" | "full" | undefined;
         roundedTop?: "small" | "medium" | "large" | "none" | "full" | undefined;
         roundedBottom?: "small" | "medium" | "large" | "none" | "full" | undefined;
-        bg?: "primary" | "secondary" | "tertiary" | "ghost" | "transparent" | "default" | "disabled" | undefined;
+        bg?: "transparent" | "primary" | "secondary" | "tertiary" | "ghost" | "default" | "disabled" | undefined;
         border?: "ghost" | undefined;
         shadow?: "button" | undefined;
     } & {
@@ -50,17 +50,17 @@ export declare const Button: {
             light?: (import("react-native").ViewStyle | import("react-native").TextStyle | import("react-native").ImageStyle) | undefined;
             dark?: (import("react-native").ViewStyle | import("react-native").TextStyle | import("react-native").ImageStyle) | undefined;
         } | undefined;
-    }, "selectors" | keyof import("react-native").PressableProps | "key" | "bg" | "border" | "shadow" | "padding" | "margin" | "mx" | "my" | "mb" | "mt" | "px" | "py" | "pb" | "pt" | "rounded" | "roundedTop" | "roundedBottom"> & React.RefAttributes<Animated.AnimatedProps<import("react-native").PressableProps & React.RefAttributes<import("react-native").View>>>>;
+    }, "selectors" | "padding" | "margin" | keyof import("react-native").PressableProps | "key" | "bg" | "border" | "shadow" | "mx" | "my" | "mb" | "mt" | "px" | "py" | "pb" | "pt" | "rounded" | "roundedTop" | "roundedBottom"> & React.RefAttributes<Animated.AnimatedProps<import("react-native").PressableProps & React.RefAttributes<import("react-native").View>>>>;
     ScaleOnPressContainer: typeof ScaleOnPressContainer;
     Text: React.ForwardRefExoticComponent<import("react-native").TextProps & {
         children?: React.ReactNode;
     } & {
-        color?: "primary" | "secondary" | "tertiary" | "ghost" | "transparent" | "default" | undefined;
+        color?: "transparent" | "primary" | "secondary" | "tertiary" | "ghost" | "default" | undefined;
         align?: "center" | undefined;
         size?: "small" | "medium" | "large" | undefined;
         leading?: "large" | undefined;
         type?: "mono" | "InterBlack" | "InterBlackItalic" | "InterBold" | "InterBoldItalic" | "InterExtraBold" | "InterExtraBoldItalic" | "InterExtraLight" | "InterExtraLightItalic" | "InterRegular" | "InterItalic" | "InterLight" | "InterLightItalic" | "InterMedium" | "InterMediumItalic" | "InterSemiBold" | "InterSemiBoldItalic" | "InterThin" | "InterThinItalic" | undefined;
-        weight?: "bold" | "medium" | "thin" | "extralight" | "light" | "normal" | "semibold" | "extrabold" | "black" | undefined;
+        weight?: "light" | "bold" | "medium" | "thin" | "extralight" | "normal" | "semibold" | "extrabold" | "black" | undefined;
     } & {
         selectors?: {
             light?: (import("react-native").ViewStyle | import("react-native").TextStyle | import("react-native").ImageStyle) | undefined;
--- packages/expo-dev-client-components/build/Text.d.ts
+++ packages/expo-dev-client-components/build/Text.d.ts
@@ -6,12 +6,12 @@ export declare const Heading: import("react").ForwardRefExoticComponent<import("
     align?: "center" | undefined;
     leading?: "large" | undefined;
     type?: "mono" | "InterBlack" | "InterBlackItalic" | "InterBold" | "InterBoldItalic" | "InterExtraBold" | "InterExtraBoldItalic" | "InterExtraLight" | "InterExtraLightItalic" | "InterRegular" | "InterItalic" | "InterLight" | "InterLightItalic" | "InterMedium" | "InterMediumItalic" | "InterSemiBold" | "InterSemiBoldItalic" | "InterThin" | "InterThinItalic" | undefined;
-    weight?: "medium" | "normal" | "bold" | "thin" | "extralight" | "light" | "semibold" | "extrabold" | "black" | undefined;
-    color?: "default" | "warning" | "error" | "link" | "secondary" | "success" | "primary" | undefined;
+    weight?: "light" | "bold" | "medium" | "thin" | "extralight" | "normal" | "semibold" | "extrabold" | "black" | undefined;
+    color?: "link" | "primary" | "secondary" | "default" | "error" | "warning" | "success" | undefined;
 } & {
     selectors?: {
-        light?: (import("react-native").TextStyle | import("react-native").ViewStyle | import("react-native").ImageStyle) | undefined;
-        dark?: (import("react-native").TextStyle | import("react-native").ViewStyle | import("react-native").ImageStyle) | undefined;
+        light?: (import("react-native").ViewStyle | import("react-native").TextStyle | import("react-native").ImageStyle) | undefined;
+        dark?: (import("react-native").ViewStyle | import("react-native").TextStyle | import("react-native").ImageStyle) | undefined;
     } | undefined;
 } & import("react").RefAttributes<import("react-native").TextProps>>;
 export declare const Text: import("react").ForwardRefExoticComponent<import("react-native").TextProps & {
@@ -21,23 +21,23 @@ export declare const Text: import("react").ForwardRefExoticComponent<import("rea
     size?: "small" | "medium" | "large" | undefined;
     leading?: "large" | undefined;
     type?: "mono" | "InterBlack" | "InterBlackItalic" | "InterBold" | "InterBoldItalic" | "InterExtraBold" | "InterExtraBoldItalic" | "InterExtraLight" | "InterExtraLightItalic" | "InterRegular" | "InterItalic" | "InterLight" | "InterLightItalic" | "InterMedium" | "InterMediumItalic" | "InterSemiBold" | "InterSemiBoldItalic" | "InterThin" | "InterThinItalic" | undefined;
-    weight?: "medium" | "normal" | "bold" | "thin" | "extralight" | "light" | "semibold" | "extrabold" | "black" | undefined;
-    color?: "default" | "warning" | "error" | "link" | "secondary" | "success" | "primary" | undefined;
+    weight?: "light" | "bold" | "medium" | "thin" | "extralight" | "normal" | "semibold" | "extrabold" | "black" | undefined;
+    color?: "link" | "primary" | "secondary" | "default" | "error" | "warning" | "success" | undefined;
 } & {
     selectors?: {
-        light?: (import("react-native").TextStyle | import("react-native").ViewStyle | import("react-native").ImageStyle) | undefined;
-        dark?: (import("react-native").TextStyle | import("react-native").ViewStyle | import("react-native").ImageStyle) | undefined;
+        light?: (import("react-native").ViewStyle | import("react-native").TextStyle | import("react-native").ImageStyle) | undefined;
+        dark?: (import("react-native").ViewStyle | import("react-native").TextStyle | import("react-native").ImageStyle) | undefined;
     } | undefined;
 } & import("react").RefAttributes<import("react-native").TextProps>>;
 export declare const TextInput: import("react").ForwardRefExoticComponent<import("react-native").TextInputProps & {
     children?: import("react").ReactNode;
 } & {
     shadow?: "input" | undefined;
-    padding?: "0.5" | "1.5" | "2.5" | "3.5" | "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("micro" | "tiny" | "small" | "medium" | "large" | "xl") | undefined;
-    px?: "0.5" | "1.5" | "2.5" | "3.5" | "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("micro" | "tiny" | "small" | "medium" | "large" | "xl") | undefined;
-    py?: "0.5" | "1.5" | "2.5" | "3.5" | "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("micro" | "tiny" | "small" | "medium" | "large" | "xl") | undefined;
-    pb?: "0.5" | "1.5" | "2.5" | "3.5" | "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("micro" | "tiny" | "small" | "medium" | "large" | "xl") | undefined;
-    pt?: "0.5" | "1.5" | "2.5" | "3.5" | "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("micro" | "tiny" | "small" | "medium" | "large" | "xl") | undefined;
+    padding?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
+    px?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
+    py?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
+    pb?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
+    pt?: "0" | "1" | "2" | "3" | "4" | "0.5" | "1.5" | "2.5" | "3.5" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96" | ("small" | "micro" | "tiny" | "medium" | "large" | "xl") | undefined;
     rounded?: "small" | "medium" | "large" | "none" | "full" | undefined;
     roundedTop?: "small" | "medium" | "large" | "none" | "full" | undefined;
     roundedBottom?: "small" | "medium" | "large" | "none" | "full" | undefined;
@@ -46,12 +46,12 @@ export declare const TextInput: import("react").ForwardRefExoticComponent<import
     size?: "small" | "medium" | "large" | undefined;
     leading?: "large" | undefined;
     type?: "mono" | "InterBlack" | "InterBlackItalic" | "InterBold" | "InterBoldItalic" | "InterExtraBold" | "InterExtraBoldItalic" | "InterExtraLight" | "InterExtraLightItalic" | "InterRegular" | "InterItalic" | "InterLight" | "InterLightItalic" | "InterMedium" | "InterMediumItalic" | "InterSemiBold" | "InterSemiBoldItalic" | "InterThin" | "InterThinItalic" | undefined;
-    weight?: "medium" | "normal" | "bold" | "thin" | "extralight" | "light" | "semibold" | "extrabold" | "black" | undefined;
-    color?: "default" | "warning" | "error" | "link" | "secondary" | "success" | "primary" | undefined;
+    weight?: "light" | "bold" | "medium" | "thin" | "extralight" | "normal" | "semibold" | "extrabold" | "black" | undefined;
+    color?: "link" | "primary" | "secondary" | "default" | "error" | "warning" | "success" | undefined;
 } & {
     selectors?: {
-        light?: (import("react-native").TextStyle | import("react-native").ViewStyle | import("react-native").ImageStyle) | undefined;
-        dark?: (import("react-native").TextStyle | import("react-native").ViewStyle | import("react-native").ImageStyle) | undefined;
+        light?: (import("react-native").ViewStyle | import("react-native").TextStyle | import("react-native").ImageStyle) | undefined;
+        dark?: (import("react-native").ViewStyle | import("react-native").TextStyle | import("react-native").ImageStyle) | undefined;
     } | undefined;
 } & import("react").RefAttributes<import("react-native").TextInputProps>>;
 //# sourceMappingURL=Text.d.ts.map
